b50bc6b55720833a682d79f9a1e3fd08b9a14409
Andezion <245122@edu.p.lodz.pl>
2025-06-03 15:31:48 +0200

    забыл это

--- main/java/module-info.java
+++ main/java/module-info.java
@@ -1,2 +1,18 @@
-module $MODULE_NAME$ {
+module Bachelor
+{
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires org.json;
+
+    opens controller to javafx.fxml;
+    opens model to javafx.base;
+    opens app to javafx.graphics;
+
+    exports controller;
+    exports app;
 }
