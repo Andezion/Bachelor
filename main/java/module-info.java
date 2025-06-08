dfa836d30184752d7ad939e860d14f6e7034e663
Andezion <245122@edu.p.lodz.pl>
2025-06-08 12:55:24 +0200

    добавил зависимости и обновил дизайн настроек

--- main/java/module-info.java
+++ main/java/module-info.java
@@ -8,11 +8,13 @@ module Bachelor
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
     requires org.json;
+    requires com.fasterxml.jackson.databind;
 
     opens controller to javafx.fxml;
-    opens model to javafx.base;
+    opens model to javafx.base, com.fasterxml.jackson.databind, javafx.fxml;
     opens app to javafx.graphics;
 
     exports controller;
     exports app;
+    exports model;
 }
